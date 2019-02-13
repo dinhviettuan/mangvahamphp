87fe22000731698c9a1ac9fe63b0fffb48941412
dinhviettuan <46431771+dinhviettuan@users.noreply.github.com>
2019-02-13 15:50:45 +0700

    add file

--- index.php
+++ index.php
@@ -1,73 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="">
-</head>
-<body>
-	<form method="post">
-  Từ: <input id = "from" type="text" name="from" placeholder="yyyyy/mm/dd"/>
-  Đến: <input id = "to" type="text" name="to" placeholder="yyyy/mm/dd" />
-  <input type = "submit" id = "submit" value = "Lọc"/>
-</form>
-<?php
-$customer_list = array(
-    "1" => array("name" => "Mai Văn Hoàn", "day_of_birth" => "1983/08/20", "address" => "Hà Nội", "profile" => "images/img1.jpg"),
-    "2" => array("name" => "Nguyễn Văn Nam", "day_of_birth" => "1983/08/21", "address" => "Bắc Giang", "profile" => "images/img2.jpg"),
-    "3" => array("name" => "Nguyễn Thái Hòa", "day_of_birth" => "1983/08/22", "address" => "Nam Định", "profile" => "images/img3.jpg"),
-    "4" => array("name" => "Trần Đăng Khoa", "day_of_birth" => "1983/08/17", "address" => "Hà Tây", "profile" => "images/img4.jpg"),
-    "5" => array("name" => "Nguyễn Đình Thi", "day_of_birth" => "1983/08/19", "address" => "Hà Nội", "profile" => "images/img5.jpg")
-);
-?>
-<?php
-function searchByDate($customers, $from_date, $to_date) {
-    if(empty($from_date) && empty($to_date)){
-        return $customers;
-    }
-    $filtered_customers = [];
-    foreach($customers as $customer){
-        if(!empty($from_date) && (strtotime($customer['day_of_birth']) < strtotime($from_date)))
-            continue;
-        if(!empty($to_date) && (strtotime($customer['day_of_birth']) > strtotime($to_date)))
-            continue;
-        $filtered_customers[] = $customer;
-    }
-    return $filtered_customers;
-}
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Using include in php</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <header>
+               <?php
+                include "header.php";
+               ?>
+            </header>
 
-$from_date = NULL;
-$to_date = NULL;
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $from_date = $_POST["from"];
-    $to_date = $_POST["to"];
-}
-$filtered_customers = searchByDate($customer_list, $from_date, $to_date);?>
-<table border="0">
-  <caption><h2>Danh sách khách hàng</h2></caption>
-  <tr>
-    <th>STT</th>
-    <th>Tên</th>
-    <th>Ngày sinh</th>
-    <th>Địa chỉ</th>
-    <th>Ảnh</th>
-  </tr>
-  <?php if(count($filtered_customers) === 0):?>
-    <tr>
-        <td colspan="5" class="message">Không tìm thấy khách hàng nào</td>
-    </tr>
-  <?php endif; ?>
+            <nav>
+               <?php
+                include "nav.php";
+               ?>
+            </nav>
 
-  <?php foreach($filtered_customers as $index=> $customer): ?>
-    <tr>
-        <td><?php echo $index + 1;?></td>
-        <td><?php echo $customer['name'];?></td>
-        <td><?php echo $customer['day_of_birth'];?></td>
-        <td><?php echo $customer['address'];?></td>
-        <td><div class="profile"><img src="<?php echo $customer['profile'];?>"/></div> </td>
-    </tr>
-    <?php endforeach; ?>
-</table>
-</body>
+            <article>
+               <?php
+                include "content.php";
+               ?>
+            </article>
+
+            <footer>
+               <?php
+                include "footer.php";
+               ?>
+            </footer>
+        </div>
+    </body>
 </html>
